84c1c7e93589d4c9c659d6f15b125a4305cf8b99
svemaraju <svemaraju11@gmail.com>
2017-07-27 21:24:19 +0530

    Input string in 16's, Mode CBC, IV is 16 empty bytes'

--- aescrypt.py
+++ aescrypt.py
@@ -2,39 +2,56 @@ from Crypto.Cipher import AES
 import base64
 import hashlib
 
-
 class PyAesCrypt(object):
-    def __init__(self,iv=None,encoding=True):
-        self.iv = iv
+    """
+    Class for implementing AES encryption and decryption in python,
+    compatible with the AESCrypt library in Android, Ruby and Objective-C
+    """
+    
+    def __init__(self,encoding=True):
+        
+        # Use 16 byte empty initialization vector for compatibility.
+        self.iv = bytes([0x00 for i in range(16)])
         self.encoding = encoding
-
+    
     def encrypt(self,key,message):
-        cipher = AES.new(key=self._hashkey(key=key))
+        
+        cipher = AES.new(self._hashkey(key=key),AES.MODE_CBC,self.iv)
         cipher_text = cipher.encrypt(self.pkcs7padding(data=message))
+        
         if self.encoding:
+            
             return base64.b64encode(s=cipher_text)
+        
         return cipher_text
-
+    
     def pkcs7padding(self,data):
-        bs = 8
+        
+        bs = 16
         padding = bs - len(data) % bs
         padding_text = chr(padding) * padding
-        return data + padding_text
-
+        padded = data + padding_text
+        return padded
+    
     def pkcs7decode(self, text):
+
         if type(text) is bytes:
             pad = ord(text.decode("utf-8")[-1])
             return text[:-pad]
         else:
             raise RuntimeError("bytes required found %s" % type(text))
-
+    
     def _hashkey(self,key):
+        
         return hashlib.sha256(key.encode()).digest()
-
+    
     def decrypt(self,key,message):
-        cipher = AES.new(key=self._hashkey(key=key))
+        
+        cipher = AES.new(self._hashkey(key=key),AES.MODE_CBC,self.iv)
+        
         if self.encoding:
             resp = cipher.decrypt(ciphertext=base64.b64decode(s=message))
         else:
             resp = cipher.decrypt(ciphertext=message)
+        
         return self.pkcs7decode(text=resp)
